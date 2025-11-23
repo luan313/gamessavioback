b0f90aaaed2d6a57998dbad647030f86b90ee3a0
LuisFcarmo <luis1170da@gmail.com>
2025-11-23 16:36:58 -0300

    liberando cors

--- app/main.py
+++ app/main.py
@@ -2,14 +2,28 @@ from fastapi import FastAPI
 from app.routers import (
     auth_router,
     backoffice,
-    user_router,
-    game_router,
-    categoria_router,
     avaliacao_router,
-    monitoramento_router,
 )
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",        
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",        
+    "https://seu-dominio.com"      
+]
 
 app = FastAPI(title="Letterboxd de Jogos")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          
+    allow_credentials=True,
+    allow_methods=["*"],           
+    allow_headers=["*"],           
+)
+
+
 app.include_router(auth_router.router, tags=["Auth"])
 app.include_router(backoffice.router, tags=["backoffice"])
 app.include_router(avaliacao_router.router, tags=["avaliação"])
