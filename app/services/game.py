ee60a531b3dd1c33dd057c78edc42311ece2e9b6
LuisFcarmo <luis1170da@gmail.com>
2025-11-27 11:36:13 -0300

    :sparkles: feat adicionando rota para buscar games por nome

--- app/services/game.py
+++ app/services/game.py
@@ -2,9 +2,16 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.game import Game
+from app.schemas.game import SearchGameResponse
 
 def get_top_hyped_games() -> list[Game]:
     return (
         select(Game)
         .order_by(Game.hype.desc())
+    )
+
+def search_games_by_name(name: str) -> list[SearchGameResponse]:
+    return (
+        select(Game)
+        .where(Game.nome.ilike(f"%{name}%"))
     )
