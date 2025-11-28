c9c62d55a90445128b54f9d08e8581c730e48516
LuisFcarmo <luis1170da@gmail.com>
2025-11-28 11:24:20 -0300

    aaa

--- app/main.py
+++ app/main.py
@@ -40,6 +40,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",        
     "http://127.0.0.1:3000",
+    "https://gamessaviofront.vercel.app/",
     "http://localhost:5173",        
 ]
 add_pagination(app)
