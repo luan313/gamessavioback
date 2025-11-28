4072845bb0fee942e69bcbbf8de367bf38f9b2fe
Luan Fernandes <luanfernandes3212@gmail.com>
2025-11-28 10:36:00 -0300

    fix url session.py

--- app/database/session.py
+++ app/database/session.py
@@ -1,10 +1,22 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from app.core.config import settings
+from typing import AsyncGenerator
+
+db_url = settings.DATABASE_URL
+
+if db_url:
+    if db_url.startswith("postgresql://"):
+        db_url = db_url.replace("postgresql://", "postgresql+asyncpg://", 1)
+    
+    if "sslmode=require" in db_url:
+        db_url = db_url.replace("?sslmode=require", "")
+
+        db_url = db_url.replace("&sslmode=require", "")
 
 engine = create_async_engine(
-    settings.DATABASE_URL,
+    db_url,
     echo=False,
-    future=True
+    future=True,
 )
 
 AsyncSessionLocal = async_sessionmaker(
@@ -14,6 +26,6 @@ AsyncSessionLocal = async_sessionmaker(
     autoflush=False,
 )
 
-async def get_db() -> AsyncSession:
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
-            yield session
+        yield session
